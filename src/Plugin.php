b71eeb5c9f55be18f9595c4f2028ecab3dd6cb2e
Matt Glaman <matt.glaman@acquia.com>
2022-05-06 09:31:35 -0500

    Move logic to PackageRequiresAdjuster (#1)

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,43 +7,20 @@ namespace ComposerDrupalLenient;
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
-use Composer\Package\AliasPackage;
-use Composer\Package\BasePackage;
-use Composer\Package\CompletePackage;
-use Composer\Package\CompletePackageInterface;
-use Composer\Package\Link;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
 use Composer\Plugin\PrePoolCreateEvent;
-use Composer\Repository\RepositoryFactory;
-use Composer\Repository\RepositoryManager;
-use Composer\Semver\Constraint\MatchAllConstraint;
-use JsonSchema\Constraints\ConstraintInterface;
 
 final class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    private PackageRequiresAdjuster $packageRequiresAdjuster;
+
     public function modifyPackages(PrePoolCreateEvent $event): void
     {
         $packages = $event->getPackages();
         foreach ($packages as $package) {
-            if ($package->getType() === 'drupal-core' || !str_starts_with($package->getType(), 'drupal-')) {
-                continue;
-            }
-            $requires = array_map(static function (Link $link) {
-                if ($link->getDescription() === Link::TYPE_REQUIRE && $link->getTarget() === 'drupal/core') {
-                    return new Link(
-                        $link->getSource(),
-                        $link->getTarget(),
-                        new MatchAllConstraint(),
-                        $link->getDescription(),
-                        (string)(new MatchAllConstraint())
-                    );
-                }
-                return $link;
-            }, $package->getRequires());
-            // @note `setRequires` is on Package but not PackageInterface.
-            if ($package instanceof CompletePackage) {
-                $package->setRequires($requires);
+            if ($this->packageRequiresAdjuster->applies($package)) {
+                $this->packageRequiresAdjuster->adjust($package);
             }
         }
         $event->setPackages($packages);
@@ -51,6 +28,8 @@ final class Plugin implements PluginInterface, EventSubscriberInterface
 
     public function activate(Composer $composer, IOInterface $io)
     {
+        // @todo set as property since we'll eventually need data from $composer.
+        $this->packageRequiresAdjuster = new PackageRequiresAdjuster();
     }
 
     public function deactivate(Composer $composer, IOInterface $io)

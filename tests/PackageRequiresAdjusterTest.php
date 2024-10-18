35d600464ed2050284de1a09a5ee0737d6f674b4
gitressa <3491208+gitressa@users.noreply.github.com>
2024-10-18 12:23:49 +0200

    Update PackageRequiresAdjusterTest.php

--- tests/PackageRequiresAdjusterTest.php
+++ tests/PackageRequiresAdjusterTest.php
@@ -87,8 +87,9 @@ class PackageRequiresAdjusterTest extends TestCase
             new Constraint('>=', '8.0'),
             new Constraint('>=', '9.0'),
             new Constraint('>=', '10.0'),
+            new Constraint('>=', '11.0'),
         ]);
-        $originalTokenConstraint = new Constraint('>=', '1.10.0');
+        $originalSimplenewsConstraint = new Constraint('>=', '4.0.0');
         $package = new CompletePackage('foo', '1.0', '1.0');
         $package->setType('drupal-module');
         $package->setRequires([
@@ -99,12 +100,12 @@ class PackageRequiresAdjusterTest extends TestCase
                 Link::TYPE_REQUIRE,
                 $originalDrupalCoreConstraint->getPrettyString()
             ),
-            'drupal/token' => new Link(
+            'drupal/simplenews' => new Link(
                 'bar',
-                'drupal/token',
-                $originalTokenConstraint,
+                'drupal/simplenews',
+                $originalSimplenewsConstraint,
                 Link::TYPE_REQUIRE,
-                $originalTokenConstraint->getPrettyString()
+                $originalSimplenewsConstraint->getPrettyString()
             )
         ]);
         $adjuster->adjust($package);
@@ -113,8 +114,8 @@ class PackageRequiresAdjusterTest extends TestCase
             $package->getRequires()['drupal/core']->getConstraint()->getPrettyString()
         );
         self::assertSame(
-            $originalTokenConstraint,
-            $package->getRequires()['drupal/token']->getConstraint()
+            $originalSimplenewsConstraint,
+            $package->getRequires()['drupal/simplenews']->getConstraint()
         );
         if ($coreVersion !== null) {
             self::assertTrue(
@@ -129,8 +130,8 @@ class PackageRequiresAdjusterTest extends TestCase
     public function provideAdjustData(): array
     {
         return [
-            [null, '^8 || ^9 || ^10 || ^11'],
-            ['10.0.0-alpha5', '^8 || ^9 || ^10 || ^11'],
+            [null, '^8 || ^9 || ^10 || ^11 || ^12'],
+            ['10.0.0-alpha5', '^8 || ^9 || ^10 || ^11 || ^12'],
         ];
     }
 }
